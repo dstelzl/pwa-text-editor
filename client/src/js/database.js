e1f0ff821b71b347918d41431716f9e1ac11dd7b
dds git <deeann@stelzl.us>
2022-10-17 09:57:46 -0400

    plug ins and scripts

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ todo: content });
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
