1dded2c42d019b2ba9bdeff53d195c30f0af5eed
dds git <deeann@stelzl.us>
2022-10-17 14:27:45 -0400

    completed database.js logic todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,14 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ todo: content });
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('data saved', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
-initdb();
